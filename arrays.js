68408bd80d6424536ac51e7544d43b91ebaea849
Shital Sanap <shital.sanap@gmail.com>
2017-09-18 17:18:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element)
+{
+  var newArray = new Array();
+  newArray = [element, ...array]
+
+  //newArray.unshift(element)
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element)
+{
+  var newArray = new Array();
+  newArray = [...array, element]
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element)
+  return array;
+}
